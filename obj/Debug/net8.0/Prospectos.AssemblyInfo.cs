0e2f4686d596653736df9319a108a265f76cde55
chrisjeriva <iscchristianrivera@gmail.com>
2024-03-13 23:09:11 -0600

    gitignore

--- obj/Debug/net8.0/Prospectos.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prospectos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prospectos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080c8c847a41894f3103a7ec8cdfaa8ea3d8c591")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prospectos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prospectos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
